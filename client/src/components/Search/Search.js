61d0c35a49bcc223665c39b497654516766364b0
ktan114 <ktaneight@gmail.com>
2018-11-26 22:52:04 -0500

    Style autocomplete bar

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
 
 import "./Search.css";
 import { API_URL } from "../../config";
@@ -10,6 +11,7 @@ class Search extends Component {
     super(props);
     this.state = {
       searchQuery: "",
+      searchResults: [],
       loading: false
     };
   }
@@ -21,15 +23,55 @@ class Search extends Component {
     fetch(`${API_URL}/autocomplete?searchQuery=${e.target.value}`)
       .then(handleResponse)
       .then(result => {
-        this.setState({ loading: false });
+        this.setState({ loading: false, searchResults: result });
       })
       .catch(err => {
         console.log(err);
       });
   };
 
+  renderSearchResults = () => {
+    const { searchResults, searchQuery, loading } = this.state;
+
+    if (!searchQuery) return "";
+
+    if (searchResults.length > 0) {
+      return (
+        <div className="Search__Result__Container">
+          {searchResults.map(result => (
+            <div
+              key={result.id}
+              className="Search__Result"
+              onClick={() => this.handleRedirect(result.id)}
+            >
+              {result.name} ({result.symbol}){" "}
+            </div>
+          ))}
+        </div>
+      );
+    }
+
+    if (!loading) {
+      return (
+        <div className="Search__Result__Container">
+          <div className="Search__No__Result">No results found.</div>
+        </div>
+      );
+    }
+  };
+
+  handleRedirect = currencyId => {
+    // Clear input value and close autocomplete container, by clearing searchQuery state
+    this.setState({
+      searchQuery: "",
+      searchResults: []
+    });
+
+    this.props.history.push(`/currency/${currencyId}`);
+  };
+
   render() {
-    const { loading } = this.state;
+    const { loading, searchQuery } = this.state;
     return (
       <div className="Search">
         <span className="Search__Icon" />
@@ -38,6 +80,7 @@ class Search extends Component {
           type="text"
           placeholder="Currency name"
           name="searchQuery"
+          value={searchQuery}
           onChange={this.handleChange}
         />
         {loading && (
@@ -45,9 +88,10 @@ class Search extends Component {
             <Loading width="12px" height="12px" />
           </div>
         )}
+        {this.renderSearchResults()}
       </div>
     );
   }
 }
 
-export default Search;
+export default withRouter(Search);
